0af1048709b986defa90f93b955ac6be6d8c24a5
Carlos Rueda <carloshrueda@gmail.com>
2018-08-13 20:06:57 -0500

    Colaboración 1 y Colaboracion 2
    Implementación de solucones Colaboración 1 y Colaboración 2

---
+++ Clases/Colaboracion1/Colaboracion1/Colaboracion1.cs
@@ -0,0 +1,111 @@
+﻿/*
+ * Problema 1:
+Un banco tiene 3 clientes que pueden hacer depósitos y extracciones. También el banco requiere que al final del día calcule la cantidad de dinero que hay depositada.
+
+Lo primero que hacemos es identificar las clases:
+
+Podemos identificar la clase Cliente y la clase Banco.
+
+Luego debemos definir los atributos y los métodos de cada clase:
+
+Cliente		
+    atributos
+        nombre
+        monto
+    métodos
+        constructor
+        Depositar
+        Extraer
+        RetornarMonto
+
+Banco
+    atributos
+        3 Cliente (3 objetos de la clase Cliente)
+    métodos
+        constructor
+        Operar
+        DepositosTotales
+Creamos un proyecto llamado: Colaboracion1 y dentro del proyecto creamos dos clases llamadas: Cliente y Banco.
+ * 
+ * */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colaboracion1
+{
+    class Cliente
+    {
+        private string nombre;
+        private int monto;
+
+        public Cliente(string nom)
+        {
+            nombre = nom;
+            monto = 0;
+        }
+
+        public void Depositar(int m)
+        {
+            monto = monto + m;
+        }
+
+        public void Extraer(int m)
+        {
+            monto = monto - m;
+        }
+
+        public int RetornarMonto()
+        {
+            return monto;
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine(nombre + " tiene depositado la suma de " + monto);
+        }
+    }
+
+    class Banco
+    {
+        private Cliente cliente1, cliente2, cliente3;
+
+        public Banco()
+        {
+            cliente1 = new Cliente("Juan");
+            cliente2 = new Cliente("Ana");
+            cliente3 = new Cliente("Pedro");
+        }
+
+        public void Operar()
+        {
+            cliente1.Depositar(100);
+            cliente2.Depositar(150);
+            cliente3.Depositar(200);
+            cliente3.Extraer(150);
+        }
+
+        public void DepositosTotales()
+        {
+            int t = cliente1.RetornarMonto() +
+                    cliente2.RetornarMonto() +
+                    cliente3.RetornarMonto();
+            Console.WriteLine("El total de dinero en el banco es:" + t);
+            cliente1.Imprimir();
+            cliente2.Imprimir();
+            cliente3.Imprimir();
+        }
+
+        static void Main(string[] args)
+        {
+            Banco banco1 = new Banco();
+            banco1.Operar();
+            banco1.DepositosTotales();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Clases/Colaboracion2/Dado/Colaboracion2.cs
@@ -0,0 +1,85 @@
+﻿/*
+ * Problema 2:
+Plantear un programa que permita jugar a los dados. Las reglas de juego son: se tiran tres dados si los tres salen con el mismo valor mostrar un mensaje que "gano", sino "perdió".
+
+Lo primero que hacemos es identificar las clases:
+
+Podemos identificar la clase Dado y la clase JuegoDeDados.
+
+Luego los atributos y los métodos de cada clase:
+
+Dado		
+    atributos
+        valor
+    métodos
+        constructor
+        Tirar
+        Imprimir
+        RetornarValor
+
+JuegoDeDados
+    atributos
+        3 Dado (3 objetos de la clase Dado)
+    métodos
+        constructor
+        Jugar
+Creamos un proyecto llamado: Colaboracion2 y dentro del proyecto creamos dos clases llamadas: Dado y JuegoDeDados.
+ * 
+ * 
+ */
+
+using System;
+
+namespace Colaboracion2
+{
+    class Dado
+    {
+        private static Random aleatorio;
+
+        public Dado() => aleatorio = new Random();
+
+        public void Tirar() => RetornarValor = aleatorio.Next(1, 7);
+
+        public void Imprimir() => Console.WriteLine("El valor del dado es:" + RetornarValor);
+
+        public int RetornarValor { get; private set; }
+    }
+
+    class JuegoDeDados
+    {
+        private Dado dado1, dado2, dado3;
+
+        public JuegoDeDados()
+        {
+            dado1 = new Dado();
+            dado2 = new Dado();
+            dado3 = new Dado();
+        }
+
+        public void Jugar()
+        {
+            dado1.Tirar();
+            dado1.Imprimir();
+            dado2.Tirar();
+            dado2.Imprimir();
+            dado3.Tirar();
+            dado3.Imprimir();
+            if (dado1.RetornarValor == dado2.RetornarValor &&
+                dado1.RetornarValor == dado3.RetornarValor)
+            {
+                Console.WriteLine("Ganó");
+            }
+            else
+            {
+                Console.WriteLine("Perdió");
+            }
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            JuegoDeDados j = new JuegoDeDados();
+            j.Jugar();
+        }
+    }
+}
