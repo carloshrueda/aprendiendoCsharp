7457b52c89ab50e5f35d516246191a97d85bb016
Carlos Rueda <carloshrueda@gmail.com>
2018-08-14 21:01:24 -0500

    Tipo de datos
    Imprimir el valor minimo y maximo de los tipos de datos

---
+++ TipoDatos/Tipo datos/Tipo datos/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tipo_datos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Valor máximo y mínimo para tipo de dato byte.");
+            Console.WriteLine("Mínimo:" + byte.MinValue);
+            Console.WriteLine("Máximo:" + byte.MaxValue);
+            Console.WriteLine("Valor máximo y mínimo para tipo de dato sbyte.");
+            Console.WriteLine("Mínimo:" + sbyte.MinValue);
+            Console.WriteLine("Máximo:" + sbyte.MaxValue);
+            Console.WriteLine("Valor mínimo y máximo para tipo de dato short.");
+            Console.WriteLine("Mínimo:" + short.MinValue);
+            Console.WriteLine("Máximo:" + short.MaxValue);
+            Console.WriteLine("Valor mínimo y máximo para tipo de dato ushort.");
+            Console.WriteLine("Mínimo:" + ushort.MinValue);
+            Console.WriteLine("Máximo:" + ushort.MaxValue);
+            Console.WriteLine("Valor mínimo y máximo para tipo de dato int.");
+            Console.WriteLine("Mínimo:" + int.MinValue);
+            Console.WriteLine("Máximo:" + int.MaxValue);
+            Console.WriteLine("Valor mínimo y máximo para tipo de dato uint.");
+            Console.WriteLine("Mínimo:" + uint.MinValue);
+            Console.WriteLine("Máximo:" + uint.MaxValue);
+            Console.WriteLine("Valor mínimo y máximo para tipo de dato long.");
+            Console.WriteLine("Mínimo:" + long.MinValue);
+            Console.WriteLine("Máximo:" + long.MaxValue);
+            Console.WriteLine("Valor mínimo y máximo para tipo de dato ulong.");
+            Console.WriteLine("Mínimo:" + ulong.MinValue);
+            Console.WriteLine("Máximo:" + ulong.MaxValue);
+            Console.ReadKey();
+        }
+    }
+}
