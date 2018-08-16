66d2396376b99af70330d09670c709eb68d211e2
Carlos Rueda <carloshrueda@gmail.com>
2018-08-16 10:41:19 -0500

    Llamadas con nombres de parámetros.
    Ejemplo de un programa con llamadas de paramétros por orden.
    Ejemplo: Implementar un método que muestre un mensaje en la pantalla indicando la fila y columna donde debe imprimirse. Luego llamar al método pasando los nombres y valores de los parámetros.

---
+++ parametros/Llamadas con nomb parametros/Llamadas con nomb parametros/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Llamadas_con_nomb_parametros
+{
+    class Program
+    {
+        public void Imprimir(string mensaje, int columna, int fila)
+        {
+            Console.SetCursorPosition(columna, fila);
+            Console.WriteLine(mensaje);
+        }
+
+        static void Main(string[] args)
+        {
+            Program ob1 = new Program();
+            ob1.Imprimir(fila: 12, columna: 40, mensaje: "Hola Mundo");
+            ob1.Imprimir(mensaje: "Fin", fila: 23, columna: 5);
+            Console.ReadKey();
+        }
+    }
+}
