d781483c9088c751e828e9f9f16f85d20d666218
Carlos Rueda <carloshrueda@gmail.com>
2018-08-16 11:01:34 -0500

    Clases estáticas

---
+++ metodos/Clases estaticas/Clases estaticas/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases_estaticas
+{
+    static class Operaciones
+    {
+        public static int Sumar(int valor1, int valor2)
+        {
+            return valor1 + valor2;
+        }
+        public static int Restar(int valor1, int valor2)
+        {
+            return valor1 - valor2;
+        }
+        public static int Multiplicar(int valor1, int valor2)
+        {
+            return valor1 * valor2;
+        }
+        public static int Dividir(int valor1, int valor2)
+        {
+            return valor1 / valor2;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("10+5 es " + Operaciones.Sumar(10, 5));
+            Console.WriteLine("10-5 es " + Operaciones.Restar(10, 5));
+            Console.WriteLine("10*5 es " + Operaciones.Multiplicar(10, 5));
+            Console.WriteLine("10/5 es " + Operaciones.Dividir(10, 5));
+            Console.ReadKey();
+        }
+    }
+}
+
+/*
+ * Una clase se la puede hacer estática siempre y cuando todos sus atributos sean estáticos, 
+ * lo mismo que sus métodos.
+ * 
+ * No se pueden crear objetos de una clase estática.
+ * 
+ * */
