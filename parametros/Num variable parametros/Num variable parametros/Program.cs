46d24cf940bb0ae3e5877af599022c5335ddd92a
Carlos Rueda <carloshrueda@gmail.com>
2018-08-16 10:29:14 -0500

    Cambios a numero variable de parametros
    Se dio solución al problema: Confeccionar un método que reciba un string con la cadena "suma" o "producto" y seguidamente una lista de enteros. El método debe retornar la suma o producto de todos los valores enviados.

--- parametros/Num variable parametros/Num variable parametros/Program.cs
+++ parametros/Num variable parametros/Num variable parametros/Program.cs
@@ -22,6 +22,32 @@ namespace Num_variable_parametros
             return su;
         }
 
+        public int Operaciones(string oper, params int[] p)
+        {
+            int res = 0;
+
+            switch (oper)
+            {
+                case "suma":
+                    res = 0;
+                    foreach (var f in p)
+                    {
+                        res += f;
+                    }
+                    break;
+
+                case "producto":
+                    res = 1;
+                    foreach (var f in p)
+                    {
+                        res *= f;
+                    }
+                    break;
+            }
+
+            return res;
+        }
+
         static void Main(string[] args)
         {
             Program p = new Program();
@@ -31,6 +57,12 @@ namespace Num_variable_parametros
             Console.WriteLine(p.Sumar(1, 9));
             Console.Write("La suma de 1,2,3,...,10 es ");
             Console.WriteLine(p.Sumar(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+            Console.Write("\nLa operacion de sumar de 1,2,3,...,5 es ");
+            Console.WriteLine(p.Operaciones("suma", 1, 2, 3, 4, 5));
+
+            Console.Write("La operacion de multiplicar de 1,2,3,...,5 es ");
+            Console.WriteLine(p.Operaciones("producto", 1, 2, 3, 4, 5));
 
             Console.ReadKey();
         }
