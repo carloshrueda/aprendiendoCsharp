ca646ed58a17933a62843b3cc4b744afe99e06f7
Carlos Rueda <carloshrueda@gmail.com>
2018-08-16 21:42:28 -0500

    Consultar Registro

---
+++ BaseDatos/ConsultarRegistro1/ConsultarRegistro1/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace ConsultarRegistro1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese código a consultar : ";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(181, 35);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(100, 20);
+            this.txtCodigo.TabIndex = 1;
+            this.txtCodigo.Enter += new System.EventHandler(this.TxtCodigo_Enter);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(181, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "&Buscar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Descripción : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Precio : ";
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Location = new System.Drawing.Point(178, 128);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(16, 13);
+            this.lblDescripcion.TabIndex = 5;
+            this.lblDescripcion.Text = ",,,";
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(178, 167);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(16, 13);
+            this.lblPrecio.TabIndex = 6;
+            this.lblPrecio.Text = "...";
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Enabled = false;
+            this.btnBorrar.Location = new System.Drawing.Point(181, 208);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(75, 23);
+            this.btnBorrar.TabIndex = 7;
+            this.btnBorrar.Text = "&Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.BtnBorrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 243);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.lblPrecio);
+            this.Controls.Add(this.lblDescripcion);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.Button btnBorrar;
+    }
+}
+
---
+++ BaseDatos/ConsultarRegistro1/ConsultarRegistro1/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+
+namespace ConsultarRegistro1
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection conexion = new SqlConnection("server=CRUEDA-PC\\SQLEXPRESS ; database=base1 ; integrated security = true");
+
+        public SqlConnection Conexion { get => conexion; set => conexion = value; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            lblDescripcion.Text = "...";
+            lblPrecio.Text = "...";
+            btnBorrar.Enabled = false;
+
+            try
+            {
+                using (SqlConnection conexion = new SqlConnection("server=CRUEDA-PC\\SQLEXPRESS ; database=base1 ; integrated security = true"))
+                {
+                    conexion.Open();
+
+                    string cod = txtCodigo.Text.Trim();
+
+                    string cadena = "select descripcion, precio from articulos where codigo=" + cod;
+
+                    SqlCommand comando = new SqlCommand(cadena, conexion);
+
+                    SqlDataReader registro = comando.ExecuteReader();
+                    if (registro.Read())
+                    {
+                        lblDescripcion.Text = registro["descripcion"].ToString();
+                        lblPrecio.Text = registro["precio"].ToString();
+                        btnBorrar.Enabled = true;
+                    }
+                    else
+                        MessageBox.Show("No existe un artículo con el código ingresado");
+
+                    conexion.Close();
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void BtnBorrar_Click(object sender, EventArgs e)
+        {
+            Conexion.Open();
+            string cod = txtCodigo.Text.Trim();
+
+            string cadena = "delete from articulos where codigo=" + cod;
+            SqlCommand comando = new SqlCommand(cadena, Conexion);
+
+            int cant;
+            cant = comando.ExecuteNonQuery();
+            if (cant == 1)
+            {
+                lblDescripcion.Text = "...";
+                lblPrecio.Text = "...";
+                MessageBox.Show("Se borró el artículo");
+            }
+            else
+                MessageBox.Show("No existe un artículo con el código ingresado");
+
+            Conexion.Close();
+
+            btnBorrar.Enabled = false;
+        }
+
+        private void TxtCodigo_Enter(object sender, EventArgs e)
+        {
+            btnBorrar.Enabled = false;
+        }
+    }
+}
