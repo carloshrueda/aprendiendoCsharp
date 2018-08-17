68f9595c82bb9071fb6c79ba073004ab6d384f50
Carlos Rueda <carloshrueda@gmail.com>
2018-08-16 21:04:03 -0500

    Operacines en una BD
    Se dan ejemplos de conexíon e inserción en una bd

---
+++ BaseDatos/BaseDatos1/BaseDatos1/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+
+namespace BaseDatos1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection conexion = new SqlConnection("server=CRUEDA-PC\\SQLEXPRESS ; database=base1 ; integrated security = true");
+            conexion.Open();
+            MessageBox.Show("Se abrió la conexión con el servidor SQL Server y se seleccionó la base de datos");
+            conexion.Close();
+            MessageBox.Show("Se cerró la conexión.");
+            conexion.Dispose();
+            conexion = null;
+        }
+    }
+}
---
+++ BaseDatos/Insert1/Insert1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Insert1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection conexion = new SqlConnection("server=CRUEDA-PC\\SQLEXPRESS ; database=base1 ; integrated security = true");
+            conexion.Open();
+            string descri = textBox1.Text.Trim();
+            string precio = textBox2.Text.Trim();
+
+            if (descri == "")
+            {
+                MessageBox.Show("La descripción no puede ser vacía");
+                return;
+            }
+
+            if (precio == "")
+            {
+                MessageBox.Show("El precio no puede ser vacío");
+                return;
+            }
+
+
+            string cadena = "insert into articulos(descripcion,precio) values ('" + descri + "'," + precio + ")";
+            SqlCommand comando = new SqlCommand(cadena, conexion);
+            comando.ExecuteNonQuery();
+            MessageBox.Show("Los datos se guardaron correctamente.");
+            textBox1.Text = "";
+            textBox2.Text = "";
+            conexion.Close();
+        }
+    }
+}
